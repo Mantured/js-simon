3e39caae18dab6f2dbc153e2a11d0a1fad538225
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-08 15:53:41 +0100

    add  while cycle + prompt

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-/* Visualizzare in pagina 5 numeri casuali.
+/*
+
+Visualizzare in pagina 5 numeri casuali.
 Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-Consigli del giorno:
-Pensate prima in italiano.
-Dividete in piccoli problemi la consegna.
-Individuate gli elementi di cui avete bisogno per realizzare il programma. */
+
+*/
+
+let number = [];
+
+let i = 0;
+while (i < 5) {
+    const randomInt = (Math.floor(Math.random() * 10));
+    console.log(randomInt);
+    number.push(randomInt);
+    i++;
+}
+
+console.log(number);
+const timer = setInterval(function () {
+    console.warn('timer started');
+}, 30000);
+
+let check = false;
+
+let numberUser = parseInt(prompt('inserisci numero'));
+console.log(typeof (numberUser));
+
+let x = 0;
+while ((x < 5) && (number.includes(numberUser))) {
+    check = true;
+    console.log('molto bene');
+    x++
+}
